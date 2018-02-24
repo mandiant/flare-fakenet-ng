124653ca6dae6a120f1af0c04682dda3b275b96b
Michael Bailey <strictlymike@gmail.com>
2018-02-24 13:48:54 -0800

    Merged PacketHandler into Diverter

--- fakenet/diverters/linux.py
+++ fakenet/diverters/linux.py
@@ -13,189 +13,6 @@ from collections import namedtuple
 from netfilterqueue import NetfilterQueue
 
 
-class PacketHandler:
-    """Used to encapsulate common patterns in packet hooks."""
-
-    def __init__(self, pkt, diverter, label, callbacks3, callbacks4):
-        self.logger = logging.getLogger('Diverter')
-
-        self.pkt = pkt
-        self.diverter = diverter  # Relies on Diverter for certain operations
-        self.label = label
-        self.callbacks3 = callbacks3
-        self.callbacks4 = callbacks4
-
-        self.raw = self.pkt.get_payload()
-        self.ipver = ((ord(self.raw[0]) & 0xf0) >> 4)
-        self.hdr, self.proto = self.diverter.parse_pkt[self.ipver](self.ipver,
-                                                                   self.raw)
-
-    def handle_pkt(self):
-        """Generic packet hook.
-
-        1.) Common prologue:
-            A.) Unconditionally Write unmangled packet to pcap
-            B.) Parse IP packet
-
-        2.) Call layer 3 (network) callbacks...
-
-        3.) Parse higher-layer protocol (TCP, UDP) for port numbers
-
-        4.) Call layer 4 (transport) callbacks...
-
-        5.) Common epilogue:
-            A.) If the packet headers have been modified:
-                i.) Double-write the mangled packet to the pcap for SSL
-                    decoding purposes
-                ii.) Update the packet payload with NetfilterQueue
-            B.) Accept the packet with NetfilterQueue
-        """
-
-        # 1A: Unconditionally write unmangled packet to pcap
-        self.diverter.write_pcap(self.hdr.pack())
-
-        if (self.hdr, self.proto) == (None, None):
-            self.logger.warning('%s: Failed to parse IP packet' % (self.label))
-        else:
-            proto_name = self.diverter.handled_protocols.get(self.proto)
-
-            self.diverter.pdebug(DGENPKT, '%s %s' % (self.label,
-                                 self.diverter.hdr_to_str(proto_name,
-                                 self.hdr)))
-
-            # 1B: Parse IP packet (actually done in ctor)
-            self.src_ip = socket.inet_ntoa(self.hdr.src)
-            self.dst_ip = socket.inet_ntoa(self.hdr.dst)
-
-            # 2: Call layer 3 (network) callbacks
-            for cb in self.callbacks3:
-                # These debug outputs are useful for figuring out which
-                # callback is responsible for an exception that was masked by
-                # python-netfilterqueue's global callback.
-                self.diverter.pdebug(DCB, 'Calling %s' % (cb))
-
-                cb(self.label, self.hdr, self.ipver, self.proto, proto_name,
-                   self.src_ip, self.dst_ip)
-
-                self.diverter.pdebug(DCB, '%s finished' % (cb))
-
-            if proto_name:
-
-                if len(self.callbacks4):
-                    # 3: Parse higher-layer protocol
-                    self.sport = self.hdr.data.sport
-                    self.dport = self.hdr.data.dport
-                    self.skey = self.diverter.gen_endpoint_key(proto_name,
-                                                               self.src_ip,
-                                                               self.sport)
-                    self.dkey = self.diverter.gen_endpoint_key(proto_name,
-                                                               self.dst_ip,
-                                                               self.dport)
-
-                    pid, comm = self.diverter.linux_get_pid_comm_by_endpoint(
-                        self.ipver, proto_name, self.src_ip, self.sport)
-
-                    if proto_name == 'UDP':
-                        fmt = '| {label} {proto} | {pid:>6} | {comm:<8} | {src:>15}:{sport:<5} | {dst:>15}:{dport:<5} | {length:>5} | {flags:<11} | {seqack:<35} |'
-                        logline = fmt.format(
-                                label=self.label,
-                                proto=proto_name,
-                                pid=pid,
-                                comm=comm,
-                                src=self.src_ip,
-                                sport=self.sport,
-                                dst=self.dst_ip,
-                                dport=self.dport,
-                                length=len(self.raw),
-                                flags='',
-                                seqack='',
-                            )
-                        self.diverter.pdebug(DGENPKTV, logline)
-
-                    elif proto_name == 'TCP':
-                        tcp = self.hdr.data
-                        # Interested in:
-                        # SYN
-                        # SYN,ACK
-                        # ACK
-                        # PSH
-                        # FIN
-                        syn = (tcp.flags & dpkt.tcp.TH_SYN) != 0
-                        ack = (tcp.flags & dpkt.tcp.TH_ACK) != 0
-                        fin = (tcp.flags & dpkt.tcp.TH_FIN) != 0
-                        psh = (tcp.flags & dpkt.tcp.TH_PUSH) != 0
-                        rst = (tcp.flags & dpkt.tcp.TH_RST) != 0
-
-                        sa = 'Seq=%d, Ack=%d' % (tcp.seq, tcp.ack)
-                        f = []
-                        if rst:
-                            f.append('RST')
-                        if syn:
-                            f.append('SYN')
-                        if ack:
-                            f.append('ACK')
-                        if fin:
-                            f.append('FIN')
-                        if psh:
-                            f.append('PSH')
-
-                        fmt = '| {label} {proto} | {pid:>6} | {comm:<8} | {src:>15}:{sport:<5} | {dst:>15}:{dport:<5} | {length:>5} | {flags:<11} | {seqack:<35} |'
-                        logline = fmt.format(
-                                label=self.label,
-                                proto=proto_name,
-                                pid=pid,
-                                comm=comm,
-                                src=self.src_ip,
-                                sport=self.sport,
-                                dst=self.dst_ip,
-                                dport=self.dport,
-                                length=len(self.raw),
-                                flags=','.join(f),
-                                seqack=sa,
-                            )
-                        self.diverter.pdebug(DGENPKTV, logline)
-
-                    if ((not (self.diverter.pdebug_level & DGENPKTV)) and
-                        pid and (pid != self.diverter.pid)):
-                        self.logger.info('  pid:  %d name: %s' %
-                                         (pid, comm if comm else 'Unknown'))
-
-                    hdr_latest = self.hdr
-                    modified = False
-
-                    # 4: Layer 4 (Transport layer) callbacks
-                    for cb in self.callbacks4:
-                        # These debug outputs are useful for figuring out which
-                        # callback is responsible for an exception that was
-                        # masked by python-netfilterqueue's global callback.
-                        self.diverter.pdebug(DCB, 'Calling %s' % (cb))
-
-                        hdr_mod = cb(self.label, pid, comm, self.ipver,
-                                     hdr_latest, proto_name,
-                                     self.src_ip, self.sport, self.skey,
-                                     self.dst_ip, self.dport, self.dkey)
-
-                        if hdr_mod:
-                            hdr_latest = hdr_mod
-                            modified = True
-
-                        self.diverter.pdebug(DCB, '%s finished' % (cb))
-
-                    if modified:
-                        # 5Ai: Double write mangled packets to represent changes
-                        # made by FakeNet-NG while still allowing SSL decoding
-                        self.diverter.write_pcap(hdr_latest.pack())
-
-                        # 5Aii: Finalize changes with nfq
-                        self.pkt.set_payload(hdr_latest.pack())
-            else:
-                self.diverter.pdebug(DGENPKT, '%s: Not handling protocol %s' %
-                                     (self.label, self.proto))
-
-        # 5B: NF_ACCEPT
-        self.pkt.accept()
-
-
 class Diverter(DiverterBase, LinUtilMixin):
 
 
@@ -306,9 +123,9 @@ class Diverter(DiverterBase, LinUtilMixin):
         # FakeNet-NG consuming large amounts of memory, contact your doctor to
         # find out if Ctrl+C is right for you.
 
-        # The below callbacks are configured to be efficiently executed by a
-        # PacketHandler object within the nonlocal, incoming, and outgoing
-        # packet hooks installed by the start method.
+        # The below callbacks are configured to be efficiently executed by the
+        # handle_pkt method, incoming, and outgoing packet hooks installed by
+        # the start method.
 
         # Network layer callbacks for nonlocal-destined packets
         #
@@ -494,9 +311,7 @@ class Diverter(DiverterBase, LinUtilMixin):
         This allows analysts to observe when malware is communicating with
         hard-coded IP addresses in MultiHost mode.
         """
-        h = PacketHandler(pkt, self, 'handle_nonlocal', self.nonlocal_net_cbs,
-                [])
-        h.handle_pkt()
+        self.handle_pkt(pkt, 'handle_nonlocal', self.nonlocal_net_cbs, [])
 
     def handle_incoming(self, pkt):
         """Incoming packet hook.
@@ -510,9 +325,8 @@ class Diverter(DiverterBase, LinUtilMixin):
 
         No return value.
         """
-        h = PacketHandler(pkt, self, 'handle_incoming', self.incoming_net_cbs,
-                self.incoming_trans_cbs)
-        h.handle_pkt()
+        self.handle_pkt(pkt, 'handle_incoming', self.incoming_net_cbs,
+                        self.incoming_trans_cbs)
 
     def handle_outgoing(self, pkt):
         """Outgoing packet hook.
@@ -529,9 +343,8 @@ class Diverter(DiverterBase, LinUtilMixin):
 
         No return value.
         """
-        h = PacketHandler(pkt, self, 'handle_outgoing', self.outgoing_net_cbs,
-                self.outgoing_trans_cbs)
-        h.handle_pkt()
+        self.handle_pkt(pkt, 'handle_outgoing', self.outgoing_net_cbs,
+                        self.outgoing_trans_cbs)
 
     def parse_ipv4(self, ipver, raw):
         hdr = dpkt.ip.IP(raw)
@@ -1047,6 +860,169 @@ class Diverter(DiverterBase, LinUtilMixin):
         hdr.sum = 0
         hdr.data.sum = 0
         str(hdr)  # This has the side-effect of invoking dpkt.in_cksum() et al
+
+    def handle_pkt(self, pkt, label, callbacks3, callbacks4):
+        """Generic packet hook.
+
+        1.) Common prologue:
+            A.) Unconditionally Write unmangled packet to pcap
+            B.) Parse IP packet
+
+        2.) Call layer 3 (network) callbacks...
+
+        3.) Parse higher-layer protocol (TCP, UDP) for port numbers
+
+        4.) Call layer 4 (transport) callbacks...
+
+        5.) Common epilogue:
+            A.) If the packet headers have been modified:
+                i.) Double-write the mangled packet to the pcap for SSL
+                    decoding purposes
+                ii.) Update the packet payload with NetfilterQueue
+            B.) Accept the packet with NetfilterQueue
+        """
+
+        raw = pkt.get_payload()
+        ipver = ((ord(raw[0]) & 0xf0) >> 4)
+        hdr, proto = self.parse_pkt[ipver](ipver, raw)
+
+        # 1A: Unconditionally write unmangled packet to pcap
+        self.write_pcap(hdr.pack())
+
+        if (hdr, proto) == (None, None):
+            self.logger.warning('%s: Failed to parse IP packet' % (label))
+        else:
+            proto_name = self.handled_protocols.get(proto)
+
+            self.pdebug(DGENPKT, '%s %s' % (label,
+                                 self.hdr_to_str(proto_name,
+                                 hdr)))
+
+            # 1B: Parse IP packet (actually done in ctor)
+            src_ip = socket.inet_ntoa(hdr.src)
+            dst_ip = socket.inet_ntoa(hdr.dst)
+
+            # 2: Call layer 3 (network) callbacks
+            for cb in callbacks3:
+                # These debug outputs are useful for figuring out which
+                # callback is responsible for an exception that was masked by
+                # python-netfilterqueue's global callback.
+                self.pdebug(DCB, 'Calling %s' % (cb))
+
+                cb(label, hdr, ipver, proto, proto_name, src_ip, dst_ip)
+
+                self.pdebug(DCB, '%s finished' % (cb))
+
+            if proto_name:
+
+                if len(callbacks4):
+                    # 3: Parse higher-layer protocol
+                    sport = hdr.data.sport
+                    dport = hdr.data.dport
+                    skey = self.gen_endpoint_key(proto_name, src_ip, sport)
+                    dkey = self.gen_endpoint_key(proto_name, dst_ip, dport)
+                    pid, comm = self.linux_get_pid_comm_by_endpoint(
+                        ipver, proto_name, src_ip, sport)
+
+                    if proto_name == 'UDP':
+                        fmt = '| {label} {proto} | {pid:>6} | {comm:<8} | {src:>15}:{sport:<5} | {dst:>15}:{dport:<5} | {length:>5} | {flags:<11} | {seqack:<35} |'
+                        logline = fmt.format(
+                                label=label,
+                                proto=proto_name,
+                                pid=pid,
+                                comm=comm,
+                                src=src_ip,
+                                sport=sport,
+                                dst=dst_ip,
+                                dport=dport,
+                                length=len(raw),
+                                flags='',
+                                seqack='',
+                            )
+                        self.pdebug(DGENPKTV, logline)
+
+                    elif proto_name == 'TCP':
+                        tcp = hdr.data
+                        # Interested in:
+                        # SYN
+                        # SYN,ACK
+                        # ACK
+                        # PSH
+                        # FIN
+                        syn = (tcp.flags & dpkt.tcp.TH_SYN) != 0
+                        ack = (tcp.flags & dpkt.tcp.TH_ACK) != 0
+                        fin = (tcp.flags & dpkt.tcp.TH_FIN) != 0
+                        psh = (tcp.flags & dpkt.tcp.TH_PUSH) != 0
+                        rst = (tcp.flags & dpkt.tcp.TH_RST) != 0
+
+                        sa = 'Seq=%d, Ack=%d' % (tcp.seq, tcp.ack)
+                        f = []
+                        if rst:
+                            f.append('RST')
+                        if syn:
+                            f.append('SYN')
+                        if ack:
+                            f.append('ACK')
+                        if fin:
+                            f.append('FIN')
+                        if psh:
+                            f.append('PSH')
+
+                        fmt = '| {label} {proto} | {pid:>6} | {comm:<8} | {src:>15}:{sport:<5} | {dst:>15}:{dport:<5} | {length:>5} | {flags:<11} | {seqack:<35} |'
+                        logline = fmt.format(
+                                label=label,
+                                proto=proto_name,
+                                pid=pid,
+                                comm=comm,
+                                src=src_ip,
+                                sport=sport,
+                                dst=dst_ip,
+                                dport=dport,
+                                length=len(raw),
+                                flags=','.join(f),
+                                seqack=sa,
+                            )
+                        self.pdebug(DGENPKTV, logline)
+
+                    if ((not (self.pdebug_level & DGENPKTV)) and
+                        pid and (pid != self.pid)):
+                        self.logger.info('  pid:  %d name: %s' %
+                                         (pid, comm if comm else 'Unknown'))
+
+                    hdr_latest = hdr
+                    modified = False
+
+                    # 4: Layer 4 (Transport layer) callbacks
+                    for cb in callbacks4:
+                        # These debug outputs are useful for figuring out which
+                        # callback is responsible for an exception that was
+                        # masked by python-netfilterqueue's global callback.
+                        self.pdebug(DCB, 'Calling %s' % (cb))
+
+                        hdr_mod = cb(label, pid, comm, ipver,
+                                     hdr_latest, proto_name,
+                                     src_ip, sport, skey,
+                                     dst_ip, dport, dkey)
+
+                        if hdr_mod:
+                            hdr_latest = hdr_mod
+                            modified = True
+
+                        self.pdebug(DCB, '%s finished' % (cb))
+
+                    if modified:
+                        # 5Ai: Double write mangled packets to represent changes
+                        # made by FakeNet-NG while still allowing SSL decoding
+                        self.write_pcap(hdr_latest.pack())
+
+                        # 5Aii: Finalize changes with nfq
+                        pkt.set_payload(hdr_latest.pack())
+            else:
+                self.pdebug(DGENPKT, '%s: Not handling protocol %s' %
+                                     (label, proto))
+
+        # 5B: NF_ACCEPT
+        pkt.accept()
 
 
 if __name__ == '__main__':
