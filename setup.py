830e7db1ca50dc1129ce84d61057526b990a95fc
strictlymike <strictlymike@gmail.com>
2019-02-06 07:25:58 -0600

    Issue 109 proto unbound local (#110)
    * Rename `proto` to `protocol` closes #109
    * Version increment for #109

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ elif platform.system().lower().startswith('linux'):
 
 setup(
     name='FakeNet NG',
-    version='1.4.2',
+    version='1.4.3',
     description="",
     long_description="",
     author="FireEye FLARE Team with credit to Peter Kacherginsky as the original developer",
