c6e015b78e462f96e0997e9d105acbd764d0c895
Michael Bailey <strictlymike@gmail.com>
2019-03-28 09:57:14 -0700

    Version bump to account for update in master

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ elif platform.system().lower().startswith('linux'):
 
 setup(
     name='FakeNet NG',
-    version='1.4.6',
+    version='1.4.7',
     description="",
     long_description="",
     author="FireEye FLARE Team with credit to Peter Kacherginsky as the original developer",
