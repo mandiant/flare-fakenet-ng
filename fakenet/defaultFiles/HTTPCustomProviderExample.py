afb634012bff337d0af273ac6d30740361682c7c
Michael Bailey <strictlymike@gmail.com>
2019-03-28 23:55:09 -0700

    Documentation and simpler code

--- fakenet/defaultFiles/HTTPCustomProviderExample.py
+++ fakenet/defaultFiles/HTTPCustomProviderExample.py
@@ -1,19 +1,28 @@
 def HandleRequest(req, method, post_data=None):
+    """Sample dynamic HTTP response handler.
+
+	Parameters
+	----------
+	req : BaseHTTPServer.BaseHTTPRequestHandler
+		The BaseHTTPRequestHandler that recevied the request
+	method: str
+		The HTTP method, either 'HEAD', 'GET', 'POST' as of this writing
+	post_data: str
+		The HTTP post data received by calling `rfile.read()` against the
+		BaseHTTPRequestHandler that received the request.
+    """
+    response = 'Ahoy\r\n'
+
     if method == 'GET':
-        response = "I'm a little teapot"
-        req.send_response(418)
-        req.send_header('X-Teapot-Type', 'ShortAndStout')
-        req.send_header('Content-Length', len(response))
-        req.end_headers()
-
-        req.wfile.write(response)
-
-    elif method == 'POST':
-        response = "Haste"
         req.send_response(200)
         req.send_header('Content-Length', len(response))
         req.end_headers()
+        req.wfile.write(response)
 
+    elif method == 'POST':
+        req.send_response(200)
+        req.send_header('Content-Length', len(response))
+        req.end_headers()
         req.wfile.write(response)
 
     elif method == 'HEAD':
